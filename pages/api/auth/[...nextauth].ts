792740a48994aa7be6e9052d8a95054553e00d29
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-19 16:28:51 +0500

    Fix nextauth trustHost error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,7 +14,6 @@ export const authOptions: NextAuthOptions = {
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    trustHost: true,
     session: {
         strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60, // 30 days
